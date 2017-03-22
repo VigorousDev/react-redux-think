286e55228ce83ef5c71a94da1739f40c361aa5e4
VigorousDev <vigorousdev@outlook.com>
2017-03-22 15:55:26 +0800

    Project page updated

--- assets/scripts/rubix/routes/Projects.js
+++ assets/scripts/rubix/routes/Projects.js
@@ -7,6 +7,7 @@ import {
   Col,
   Grid,
   Icon,
+  Button,
   Label,
   Panel,
   Table,
@@ -16,8 +17,52 @@ import {
   PanelContainer,
 } from '@sketchpixy/rubix';
 
-class DatatableComponent extends React.Component {    
+class DatatableComponent extends React.Component {  
+    constructor(props){
+        super(props);  
+        console.log('constructor');
+        var projects = [
+                {
+                    'current': 'Active',
+                    'name': 'It\'s A Wonderful Life',
+                    'time': 'Created 01.08.2017 10:04AM',
+                    'users': 8,
+                    'owner': {
+                        'picture': '/imgs/app/avatars/avatar0.png',
+                        'name': 'John Smith'
+                    }
+                },
+                {
+                    'current': 'Switch',
+                    'name': 'Casablanca',
+                    'time': 'Created 01.09.2017 09:23AM',
+                    'users': 3,
+                    'owner': {
+                        'picture': '/imgs/app/avatars/avatar0.png',
+                        'name': 'John Smith'
+                    }
+                },
+                {
+                    'current': 'Switch',
+                    'name': 'The Wizard of Oz',
+                    'time': 'Created 01.09.2017 09:23AM',
+                    'users': 28,
+                    'owner': {
+                        'picture': '/imgs/app/avatars/avatar1.png',
+                        'name': 'Alex Smith'
+                    }
+                }
+            ];
+        this.state = {
+            projects:projects,
+            datatable: null
+        };
+    }
     componentDidMount() {
+        console.log('componentDidMount');
+        this.initializeDatatable();
+    }
+    initializeDatatable(){
         var rows_selected = [];
         var table = $(ReactDOM.findDOMNode(this.example))
         .DataTable({
@@ -118,10 +163,11 @@ class DatatableComponent extends React.Component {
         table.on('draw', function(){
             // Update state of "Select all" control
             that.updateDataTableSelectAllCtrl(table);
-        });
+        });   
+        this.setState({datatable: table});
     }
 
-  updateDataTableSelectAllCtrl(table){
+    updateDataTableSelectAllCtrl(table){
         var $table             = table.table().node();
         var $chkbox_all        = $('tbody input[type="checkbox"]', $table);
         var $chkbox_checked    = $('tbody input[type="checkbox"]:checked', $table);
@@ -150,80 +196,70 @@ class DatatableComponent extends React.Component {
         }
     }
 
-  render() {
-    var projects = [
-        {
-            'current': 'Active',
-            'name': 'It\'s A Wonderful Life',
-            'time': 'Created 01.08.2017 10:04AM',
-            'users': 8,
-            'owner': {
-                'picture': '/imgs/app/avatars/avatar0.png',
-                'name': 'John Smith'
+    prompt(index) {
+        var {projects,datatable} = this.state; 
+        
+        vex.dialog.prompt({
+            message: 'Edit Project',
+            content: 'djflkd',
+            placeholder: projects[index].name,
+            callback: (value) => {
+                projects[index].name = value;
+                this.setState({projects: projects});
+                datatable.rows().invalidate().draw();
             }
-        },
-        {
-            'current': 'Switch',
-            'name': 'Casablanca',
-            'time': 'Created 01.09.2017 09:23AM',
-            'users': 3,
-            'owner': {
-                'picture': '/imgs/app/avatars/avatar0.png',
-                'name': 'John Smith'
-            }
-        },
-        {
-            'current': 'Switch',
-            'name': 'The Wizard of Oz',
-            'time': 'Created 01.09.2017 09:23AM',
-            'users': 28,
-            'owner': {
-                'picture': '/imgs/app/avatars/avatar1.png',
-                'name': 'Alex Smith'
-            }
-        }
-    ]
-    var rows = _.map(projects, function(p, index){
+        });
+    }
+
+    render() {
+        console.log('render');
+        var {projects} = this.state;
+        var that = this;
+        var rows = _.map(projects, function(p, index){
+            console.log('render - ', index);
+            return (
+                <tr key={index}>
+                    <td>{index+1}</td>
+                    <td><Label className={p.current=='Active' ? 'bg-darkgreen45 fg-white' : 'bg-darkgray25 fg-black'}>{p.current}</Label></td>
+                    <td><strong>{p.name}</strong><br/><small>{p.time}</small></td>
+                    <td>{p.users} Users</td>
+                    <td>
+                        <Row>
+                            <Col xs={3} collapseRight>
+                                <img src={p.owner.picture} width='30' height='30' />
+                            </Col>
+                            <Col xs={9} collapseLeft>
+                                <div style={{top: 8, fontSize: 16, lineHeight: 1, position: 'relative'}}>{p.owner.name}</div>
+                            </Col>
+                        </Row>
+                    </td>
+                    <td>
+                        <Button onClick={that.prompt.bind(that, index)}>
+                            <Icon style={{fontSize: 12}} glyph={'icon-fontello-pencil-1'} />
+                            Edit
+                        </Button>
+                    </td>
+                </tr>
+            )
+        });
         return (
-            <tr key={index}>
-                <td>{index+1}</td>
-                <td><Label className={p.current=='Active' ? 'bg-darkgreen45 fg-white' : 'bg-darkgray25 fg-black'}>{p.current}</Label></td>
-                <td><strong>{p.name}</strong><br/><small>{p.time}</small></td>
-                <td>{p.users} Users</td>
-                <td>
-                    <Row>
-                        <Col xs={3} collapseRight>
-                            <img src={p.owner.picture} width='30' height='30' />
-                        </Col>
-                        <Col xs={9} collapseLeft>
-                            <div style={{top: 8, fontSize: 16, lineHeight: 1, position: 'relative'}}>{p.owner.name}</div>
-                        </Col>
-                    </Row>
-                </td>
-                <td>
-                    <button type="button"><Icon style={{fontSize: 12}} glyph={'icon-fontello-pencil-1'} />Edit</button>
-                </td>
-            </tr>
-        )
-    });
-    return (
-      <Table ref={(c) => this.example = c} id='example' className='display' cellSpacing='0' width='100%'>
-        <thead>
-          <tr>
-            <th><input name="select_all" value="1" type="checkbox"/></th>
-            <th>Current</th>
-            <th>Project Name</th>
-            <th>Users</th>
-            <th>Owner</th>
-            <th>Edit</th>
-          </tr>
-        </thead>
-        <tbody>
-          {rows}
-        </tbody>
-      </Table>
-    );
-  }
+            <Table ref={(c) => this.example = c} id='example' className='display' cellSpacing='0' width='100%'>
+                <thead>
+                <tr>
+                    <th><input name="select_all" value="1" type="checkbox"/></th>
+                    <th>Current</th>
+                    <th>Project Name</th>
+                    <th>Users</th>
+                    <th>Owner</th>
+                    <th>Edit</th>
+                </tr>
+                </thead>
+                <tbody>
+                {rows}
+                </tbody>
+            </Table>
+        );
+    }
 }
 
 export default class Datatablesjs extends React.Component {
