a4c271bdcd0c67823e42e50a5dbd497250150e95
VigorousDev <vigorousdev@outlook.com>
2017-04-04 05:18:12 +0800

    Breakdown updated.

--- assets/scripts/rubix/routes/schedule/Breakdown.js
+++ assets/scripts/rubix/routes/schedule/Breakdown.js
@@ -9,7 +9,11 @@ import {
   ButtonToolbar,
   ButtonGroup,
   DropdownHoverButton,
-  MenuItem
+  MenuItem,
+  Form,
+  FormControl,
+  FormGroup,
+  ControlLabel
 } from '@sketchpixy/rubix';
 
 export class Breakdown extends React.Component {
@@ -21,14 +25,22 @@ export class Breakdown extends React.Component {
 
   }
 
+  handleChange(key, e) {
+    // let newValue = e.target.value;
+    // let {data} = this.state
+    // if(!data) data = {};
+    // data[key] = newValue;
+    // this.setState({data: data});
+  }
+
   render(){
     return (
-      <div>
+      <div className="page-breakdown">
         <ButtonToolbar>
           <ButtonGroup sm>
             <DropdownHoverButton id="bg-nested-dropdown" bsStyle='green' 
               title={<Icon style={{fontSize: 12, color: 'white'}} glyph={'icon-fontello-menu-1'} />}>
-              <MenuItem active eventKey="1"><Icon glyph={'icon-fontello-newspaper'} />&nbsp;Element Manager</MenuItem>
+              <MenuItem eventKey="1"><Icon glyph={'icon-fontello-newspaper'} />&nbsp;Element Manager</MenuItem>
               <MenuItem eventKey="2"><Icon glyph={'icon-fontello-th-list-5'} />&nbsp;Stripboard Manager</MenuItem>
               <MenuItem eventKey="3"><Icon glyph={'icon-fontello-calendar-1'} />&nbsp;Calendar Manager</MenuItem>
               <MenuItem divider />
@@ -63,6 +75,121 @@ export class Breakdown extends React.Component {
             </Button>
           </ButtonGroup>
         </ButtonToolbar>
+        <Form className='frm_breakdown'>
+          <FormGroup controlId="frm_bdName">
+            <FormControl type="text" className="text-center" style={{fontWeight:'bold'}} value="202PT1" onChange={::this.handleChange}/>
+          </FormGroup>
+          <Row>
+            <Col sm={2}>
+              <FormGroup controlId="frm_bdIntExt">
+                <ControlLabel className="text-center">Int/Ext</ControlLabel>
+                <FormControl componentClass="select" defaultValue="" onChange={::this.handleChange}>
+                  <option value="int">INT</option>
+                  <option value="ext">EXT</option>
+                  <option value="all">I/E</option>
+                </FormControl>
+              </FormGroup>
+            </Col>
+            <Col sm={8}>
+              <FormGroup controlId="frm_bdSet">
+                <ControlLabel>Set</ControlLabel>
+                <FormControl type="text" value="GEORGE'S OFFICE" onChange={::this.handleChange}/>
+              </FormGroup>
+            </Col>
+            <Col sm={2}>
+              <FormGroup controlId="frm_bdDayNight">
+                <ControlLabel className="text-center">Day/Night</ControlLabel>
+                <FormControl componentClass="select" placeholder="DAY">
+                  <option value="day">DAY</option>
+                  <option value="night">NIGHT</option>
+                  <option value="dawn">DAWN</option>
+                  <option value="dusk">DUSK</option>
+                  <option value="morning">MORNING</option>
+                  <option value="afternoon">AFTERNOON</option>
+                  <option value="evening">EVENING</option>
+                  <option value="magichour">MAGIC HOUR</option>
+                  <option value="space">SPACE</option>
+                </FormControl>
+              </FormGroup>
+            </Col>
+          </Row>
+          <Row>
+            <Col sm={10}>
+              <FormGroup controlId="frm_bdDescription">
+                <ControlLabel>Description</ControlLabel>
+                <FormControl type="text" onChange={::this.handleChange} value="You can type quite a lot of text in the description field compared to MMS."/>
+              </FormGroup>
+            </Col>
+            <Col sm={1}>
+              <FormGroup controlId="frm_bdPages">
+                <ControlLabel className="text-center">Pages</ControlLabel>
+                <FormControl type="text" value="100" onChange={::this.handleChange}/>
+              </FormGroup>
+            </Col>
+            <Col sm={1}>
+              <FormGroup controlId="frm_bd18">
+                <ControlLabel className="text-center">1/8</ControlLabel>
+                <FormControl componentClass="select" onChange={::this.handleChange}>
+                  <option value="0">0</option>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option>
+                  <option value="6">6</option>
+                  <option value="7">7</option>
+                  <option value="8">8</option>
+                </FormControl>
+              </FormGroup>
+            </Col>
+          </Row>
+          <Row>
+            <Col sm={2}>
+              <FormGroup controlId="frm_bdScriptPg">
+                <ControlLabel className="text-center">Script Pg</ControlLabel>
+                <FormControl type="text" value="100AB" onChange={::this.handleChange}/>
+              </FormGroup>
+            </Col>
+            <Col sm={2}>
+              <FormGroup controlId="frm_bdScriptPg">
+                <ControlLabel className="text-center">Script Day</ControlLabel>
+                <FormControl type="text" value="Night 100" onChange={::this.handleChange}/>
+              </FormGroup>
+            </Col>
+            <Col sm={3}>
+              <FormGroup controlId="frm_bdUnit">
+                <ControlLabel className="text-center">Unit</ControlLabel>
+                <FormControl type="text" value="Splinter Unit" onChange={::this.handleChange}/>
+              </FormGroup>
+            </Col>
+            <Col sm={3}>
+              <FormGroup controlId="frm_bdLocation">
+                <ControlLabel className="text-center">Location</ControlLabel>
+                <FormControl type="text" value="Savings & Loan" onChange={::this.handleChange}/>
+              </FormGroup>
+            </Col>
+            <Col sm={1}>
+              <FormGroup controlId="frm_bdHour">
+                <ControlLabel className="text-center">Hour</ControlLabel>
+                <FormControl type="text" value="10" onChange={::this.handleChange}/>
+              </FormGroup>
+            </Col>
+            <Col sm={1}>
+              <FormGroup controlId="frm_bdHour">
+                <ControlLabel className="text-center">Mins</ControlLabel>
+                <FormControl type="text" value="30" onChange={::this.handleChange}/>
+              </FormGroup>
+            </Col>
+          </Row>
+          <Row>
+            <Col sm={12}>
+              <FormGroup controlId="frm_bdComments">
+                <ControlLabel>Comments</ControlLabel>
+                <FormControl type="text" onChange={::this.handleChange} value="This is an extremely very long comment that I am making about this breakdown sheet right now."/>
+              </FormGroup>
+            </Col>
+          </Row>
+        </Form>
       </div>
     );
   }
